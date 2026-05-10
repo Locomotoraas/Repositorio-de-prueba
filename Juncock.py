f2bf7f197a094372d23aff31f76f22faa040dc28
ELGORILAKINGKOOOONG <juanpozzo7@gmail.com>
2026-05-10 11:41:49 -0300

    Cambio en la segunda rama equipo de trabajo V2

--- Juncock.py
+++ Juncock.py
@@ -3,5 +3,6 @@ import time
 print("Te parecen bien 47 centimetros?")
 input(": ")
 print("No me importa lo que digas, ven aqui, JAKSDJASKDJASDJASDAJDAJS")
-time.sleep(5)
+time.sleep(3)
 print("PD: Los que les gusta el dark romance no tienen amor propio")
+a = 1
---
+++ WEBON.py
@@ -0,0 +1,2 @@
+print("Esta es las V1, del trabajador principal, la main se queda de rama principal para que unan sus ramas a esta, y asi no se pierde nada, y se mantiene el orden")
+V = 3
