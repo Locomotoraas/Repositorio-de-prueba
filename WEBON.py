e78b8cb202fd53ddfffb7cea6a388dcd619af40b
NoMeGustanLosTrenes <juanpozzo7@gmail.com>
2026-05-10 16:48:38 -0300

    Fix merge conflict in WEBON.py
    Removed merge conflict markers and cleaned up imports.

--- WEBON.py
+++ WEBON.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
+from ast import Lambda
 print("Esta es las V1, del trabajador principal, la main se queda de rama principal para que unan sus ramas a esta, y asi no se pierde nada, y se mantiene el orden")
 V = 3
-=======
-from ast import Lambda
-
-
 Vb2 = 12
 complex = 1 - 1j
 print(Vb2)
 print(complex)
 assert Vb2 == 12
 print(function(5))
->>>>>>> main
+
