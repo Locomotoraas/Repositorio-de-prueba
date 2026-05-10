8d4c9c7ff5688c0f0bbc0c1d73f3f30f2182a941
ELGORILAKINGKOOOONG <juanpozzo7@gmail.com>
2026-05-10 11:04:14 -0300

    El sabroseo de Juncock (Lost media papi)

--- Juncock.py
+++ Juncock.py
@@ -1,2 +1,7 @@
+import time
+
 print("Te parecen bien 47 centimetros?")
 input(": ")
+print("No me importa lo que digas, ven aqui, JAKSDJASKDJASDJASDAJDAJS")
+time.sleep(5)
+print("PD: Los que les gusta el dark romance no tienen amor propio")
